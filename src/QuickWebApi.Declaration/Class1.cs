c4dfa0c82cde9ffd50f13c915e2dabc08d401307
winhu <winhu80@gmail.com>
2016-05-30 14:39:23 +0800

    upgrade 1st

---
+++ src/QuickWebApi.Declaration/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickWebApi.Declaration
+{
+    public enum MethodType
+    {
+        NONE = 0,
+        HTTPGET = 1,
+        HTTPPOST = 2,
+        HTTPPUT = 4,
+        HTTPDEL = 8
+    }
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class QuickWebApiDllAttribute : Attribute
+    {
+        public QuickWebApiDllAttribute(string name, string domain) { _name = name; _domain = domain; }
+        string _name = "default", _domain = "http://localhost";
+
+        public string Name { get { return _name; } }
+        public string Domain { get { return _domain; } }
+    }
+
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Method)]
+    public class QuickWebApiAttribute : Attribute
+    {
+        public QuickWebApiAttribute(string service, string route, string name = null, string comment = null)
+            : this(service, route, MethodType.NONE, name, comment)
+        { }
+        public QuickWebApiAttribute(MethodType methodtype, string name = null, string comment = null)
+            : this(null, null, methodtype, name, comment)
+        { }
+        public QuickWebApiAttribute(string service, string route, MethodType methodtype = MethodType.NONE, string name = null, string comment = null)
+        {
+            //if (string.IsNullOrWhiteSpace(server)) throw new ArgumentNullException("server can not be empty");
+            _service = service;
+            _name = name;
+            _comment = comment;
+            _methodtype = methodtype;
+            _route = route;
+        }
+        string _service, _name, _comment, _route;
+        MethodType _methodtype;
+        public string service { get { return _service; } }
+        public MethodType methodtype { get { return _methodtype; } }
+        public string name { get { return _name; } }
+        public string comment { get { return _comment; } }
+        public string route { get { return _route; } }
+    }
+}
---
+++ src/QuickWebApi.Declaration/result.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickWebApi.Declaration
+{
+
+    /// <summary>
+    /// commonly used result,
+    /// successful when errcode=0
+    /// </summary>
+    public class result<T> where T : class, new()
+    {
+        /// <summary>
+        /// commonly used result（errcode=0）
+        /// </summary>
+        public result() : this(0, null, -9999, null) { }
+        /// <summary>
+        /// commonly used result（errcode=-1 when data=null）
+        /// </summary>
+        public result(T data) : this(data == null ? -1 : 0, null, -9999, data) { }
+        /// <summary>
+        /// commonly used result（errcode=0 when errmsg is null or empty）
+        /// </summary>
+        public result(string errmsg) : this(string.IsNullOrWhiteSpace(errmsg) ? 0 : -1, errmsg, -9999, null) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">successful when errcode=0</param>
+        /// <param name="errmsg">err msg</param>
+        public result(int errcode, string errmsg = null) : this(errcode, errmsg, -9999, null) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">successful when errcode=0</param>
+        /// <param name="id">if you need, default value is 0</param>
+        public result(int errcode, long id) : this(errcode, null, id, null) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">successful when errcode=0</param>
+        /// <param name="data">if you need, default value is null</param>
+        public result(int errcode, T data) : this(errcode, null, -9999, data) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">commonly used result</param>
+        /// <param name="errmsg">err msg</param>
+        /// <param name="id">if you need</param>
+        /// <param name="data">if you need</param>
+        public result(int errcode, string errmsg, long id, T data)
+        {
+            this.errcode = errcode;
+            this.errmsg = errmsg;
+            this.id = id;
+            this.data = data;
+            time = DateTime.Now;
+        }
+        /// <summary>
+        /// indecate successful or fail
+        /// </summary>
+        public int errcode { get; set; }
+        /// <summary>
+        /// msg for result
+        /// </summary>
+        public string errmsg { get; set; }
+        /// <summary>
+        /// another result type if you need
+        /// </summary>
+        public long id { get; set; }
+        /// <summary>
+        /// result time
+        /// </summary>
+        public DateTime time { get; set; }
+        /// <summary>
+        /// another result type if you need
+        /// </summary>
+        public T data { get; set; }
+
+        /// <summary>
+        /// indecate success or fail
+        /// </summary>
+        /// <returns></returns>
+        public bool OK() { return errcode == 0; }
+
+        public override string ToString()
+        {
+            return string.Format("errcode={0},errmsg={1},id={2},data={3},time={4}",
+                errcode, errmsg, id, data == null ? string.Empty : data.ToString(), time.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+
+
+    /// <summary>
+    /// commonly used result,
+    /// successful when errcode=0
+    /// </summary>
+    public class result : result<object>
+    {
+        /// <summary>
+        /// commonly used result（errcode=0）
+        /// </summary>
+        public result() : this(0, null, -9999, null) { }
+        /// <summary>
+        /// commonly used result（errcode=-1 when data=null）
+        /// </summary>
+        public result(object data) : this(data == null ? -1 : 0, null, -9999, data) { }
+        /// <summary>
+        /// commonly used result（errcode=0 when errmsg is null or empty）
+        /// </summary>
+        public result(string errmsg) : this(string.IsNullOrWhiteSpace(errmsg) ? 0 : -1, errmsg, -9999, null) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">successful when errcode=0</param>
+        /// <param name="errmsg">err msg</param>
+        public result(int errcode, string errmsg = null) : this(errcode, errmsg, -9999, null) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">successful when errcode=0</param>
+        /// <param name="id">if you need, default value is 0</param>
+        public result(int errcode, long id) : this(errcode, null, id, null) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">successful when errcode=0</param>
+        /// <param name="data">if you need, default value is null</param>
+        public result(int errcode, object data) : this(errcode, null, -9999, data) { }
+        /// <summary>
+        /// commonly used result
+        /// </summary>
+        /// <param name="errcode">commonly used result</param>
+        /// <param name="errmsg">err msg</param>
+        /// <param name="id">if you need</param>
+        /// <param name="data">if you need</param>
+        public result(int errcode, string errmsg, long id, object data)
+            : base(errcode, errmsg, id, data)
+        { }
+    }
+}
